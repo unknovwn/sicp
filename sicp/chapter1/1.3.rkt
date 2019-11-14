711ba77dbf09119de168b51fb0289ddbd61178a4
Smirnov Dmitry <danacku@gmail.com>
2019-11-15 01:04:18 +0300

    Add 1.3 exercise

---
+++ sicp/chapter1/1.3.rkt
@@ -0,0 +1,18 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+
+(define (square-largest-sum a b c)
+  (cond ((and (>= a c) (>= b c)) (sum-of-squares a b))
+        ((and (>= a b) (>= c b)) (sun-of-squares a c))
+        ((and (>= b a) (>= c a)) (sum-of-squares b c))))
+
+(check-equal? (square-largest-sum 1 2 3) 13)
+(check-equal? (square-largest-sum 1 1 1) 2)
+(check-equal? (square-largest-sum 1 2 2) 8)
+(check-equal? (square-largest-sum 3 3 2) 18)
