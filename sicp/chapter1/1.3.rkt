d02a6ab40db7f223ddfba48262675c37407e19bb
Dmitry Smirnov <34074848+unknovwn@users.noreply.github.com>
2019-11-15 01:05:29 +0300

    Fix 1.3

--- sicp/chapter1/1.3.rkt
+++ sicp/chapter1/1.3.rkt
@@ -9,7 +9,7 @@
 
 (define (square-largest-sum a b c)
   (cond ((and (>= a c) (>= b c)) (sum-of-squares a b))
-        ((and (>= a b) (>= c b)) (sun-of-squares a c))
+        ((and (>= a b) (>= c b)) (sum-of-squares a c))
         ((and (>= b a) (>= c a)) (sum-of-squares b c))))
 
 (check-equal? (square-largest-sum 1 2 3) 13)
