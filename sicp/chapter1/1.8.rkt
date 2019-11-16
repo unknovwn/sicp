f1fb47c69917c0373238035959094a75ffdc844b
Smirnov Dmitry <danacku@gmail.com>
2019-11-17 02:23:53 +0300

    Add 1.8 exercise

--- sicp/chapter1/1.8.rkt
+++ sicp/chapter1/1.8.rkt
@@ -11,19 +11,21 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-(define (sqrt-iter guess oldguess x)
+(define (cbrt-iter guess oldguess x)
   (if (good-enough? guess oldguess)
       guess
-      (sqrt-iter (improve guess x)
+      (cbrt-iter (improve guess x)
                  guess
                  x)))
 
-(define (improve guess x)
-  (average guess (/ x guess)))
-
 (define (good-enough? guess oldguess)
   (<= (abs (- guess oldguess))
-    (* guess 0.001)))
+      (* guess 0.001)))
 
-(define (sqrt x)
-  (sqrt-iter 1.0 2.0 x))
+(define (improve guess x)
+  (/ (+ (/ x (square guess))
+        (* 2 guess))
+     3))
+
+(define (cbrt x)
+  (cbrt-iter 1.0 2.0 x))
