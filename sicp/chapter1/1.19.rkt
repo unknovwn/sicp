974cc65e3e7f1c41e6bf1de365d0d0294428fcf9
Smirnov Dmitry <danacku@gmail.com>
2019-11-22 01:56:22 +0300

    Add 1.19 exercise

---
+++ sicp/chapter1/1.19.rkt
@@ -0,0 +1,30 @@
+#lang sicp
+
+(#%require rackunit)
+
+(define (square x) (* x x))
+
+(define (even? x)
+  (= (remainder x 2) 0))
+
+(define (fib n)
+  (define (fib-iter a b p q count)
+    (cond ((= count 0) b)
+          ((even? count)
+           (fib-iter a
+                     b
+                     (+ (square p) (square q))
+                     (+ (* 2 p q) (square q))
+                     (/ count 2)))
+          (else (fib-iter (+ (* b q) (* a q) (* a p))
+                          (+ (* b p) (* a q))
+                          p
+                          q
+                          (- count 1)))))
+  (fib-iter 1 0 0 1 n))
+
+(check-equal? (fib 0) 0)
+(check-equal? (fib 1) 1)
+(check-equal? (fib 2) 1)
+(check-equal? (fib 5) 5)
+(check-equal? (fib 8) 21)
